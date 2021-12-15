464405d5ca45df068ba18b5a82253200c5a4aa04
Abdullaev-kosimzho-002 <dzukosta348@gmail.com>
2021-12-15 17:58:48 +0300

    Integer_homework

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Int_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                   //Integer 2.
+        //     int M = Convert.ToInt32(Console.ReadLine());
+        //     int mas_1 = M / 1000;
+        //     Console.WriteLine(mas_1);
+        //     
+        //
+        //
+        
+        //Integer 4.
+        // int a = Convert.ToInt16(Console.ReadLine());
+        // int b = Convert.ToInt16(Console.ReadLine());
+        // int all = a / b;
+        // Console.WriteLine(all);
+        //
+        //
+
+        //Integer 5.
+        
+        // int A = Convert.ToInt32(Console.ReadLine());
+        // int B = Convert.ToInt32(Console.ReadLine());
+        //
+        // int answer = A % B;
+        // Console.WriteLine(answer);
+        //
+        //
+ 
+        //Integer 6.
+
+        // int firstNumber_1 = 35;
+        // Console.ReadLine();
+        // Console.WriteLine(firstNumber_1);
+        //
+        //
+        //
+
+         //Integer 7.
+
+         // int number_10 = 37;
+         // int plusNumber = 3 + 7;
+         // Console.WriteLine(plusNumber);
+         // int multiply_1 = 3 * 7;
+         // Console.WriteLine(multiply_1);
+         //
+         //
+         //
+         //
+
+         //Integer 3.
+         // int nasa = Convert.ToInt32(Console.ReadLine());
+         // int result_3 = nasa / 1024;
+         // Console.WriteLine(result_3);
+         //
+
+        }
+    }
+}
